5031d941d56ba9759a030b2cd11ca46b6fb14418
PGrasseels <grasseelsp@hotmail.com>
2019-11-09 22:54:55 +0100

    Add save on json and clean

--- src/common/common-functions.js
+++ src/common/common-functions.js
@@ -2,8 +2,9 @@
 
 const CognitiveServicesCredentials = require("@azure/ms-rest-js");
 const TextAnalyticsAPIClient = require("@azure/cognitiveservices-textanalytics");
+const fs = require('fs');
 
-async function analyzeContent(text){
+async function analyzeContent(text) {
     const subscription_key = '43ef3ee75b274baca613fce15bafe9f9';
     const endpoint = 'https://wetryconf.cognitiveservices.azure.com/';
 
@@ -13,7 +14,7 @@ async function analyzeContent(text){
     return await sentimentAnalysis(textAnalyticsClient, text);
 };
 
-async function sentimentAnalysis(client, text){
+async function sentimentAnalysis(client, text) {
 
     const sentimentInput = {
         documents: [
@@ -24,10 +25,34 @@ async function sentimentAnalysis(client, text){
     const sentimentResult = await client.sentiment({
         multiLanguageBatchInput: sentimentInput
     });
-    
+
     return sentimentResult;
 };
 
+function signInUser(username, password) {
+    return username === 'admin' && password === 'admin';
+}
+
+function cleanItems() {
+    var items = getGoldenBookItems();
+}
+
+// Helpers
+function getGoldenBookItems() {
+    let rawdata = fs.readFileSync('book.json');
+    return JSON.parse(rawdata);
+}
+
+function writeGoldenBookItems(items) {
+    let data = JSON.stringify(items);
+    fs.writeFileSync('book.json', data);
+}
+
+function insertGoldenBookItem(item) {
+    var items = getGoldenBookItems();
+    items.data.push(item);
+    writeGoldenBookItems(items);
+}
 
 // For test -> node .\common-functions.js
 // { documents: [ { id: '1', score: 0.6132941842079163 } ], errors: [] }
@@ -36,3 +61,7 @@ analyzeContent('Je vous aime pour toujours').then(
         console.log(response);
     }
 );
+
+insertGoldenBookItems({ title: 'My Title', content: 'Je vous aime pour toujours ! Vous êtez geniaux !', createAt: Date.now() });
+
+console.log(getGoldenBookItems());
